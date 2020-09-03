5903944437ca9a0a192efafc8ebf615ede97bd1d
Christo-IV <christo.vahtramae@khk.ee>
2020-09-03 14:25:29 +0300

    On koostatud Laul klass ja testitud antud klassi objekti loomist

---
+++ Menu.py
@@ -0,0 +1,5 @@
+from laul import Laul
+
+# Laulu objekti testimine
+laul_test = Laul("Gajin", "MoMo")
+print(laul_test.pealkiri, laul_test.artist)
---
+++ laul.py
@@ -0,0 +1,11 @@
+class Laul:
+    """ Klass laul
+
+    Klassi omadused (Attributes):
+    p_pealkiri (str) - laulu pealkiri
+    p_artist (str) - laulu artist
+    """
+
+    def __init__(self,p_pealkiri,p_artist ):
+        self.pealkiri = p_pealkiri
+        self.artist = p_artist
